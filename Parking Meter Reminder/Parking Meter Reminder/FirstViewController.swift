6f05c6d92946159e6343285d4c386d270e6eace9
Kunal Botla <kunalbotla@gmail.com>
2017-11-05 18:34:35 -0500

    Major Updates
    Timer almost done. Location in progess.

--- Parking Meter Reminder/Parking Meter Reminder/FirstViewController.swift
+++ Parking Meter Reminder/Parking Meter Reminder/FirstViewController.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
+import UserNotifications
 
 class FirstViewController: UIViewController {
-//New Code
-    
     //Objects
     @IBOutlet weak var timeDisplay: UILabel!
     
-    var seconds = 60
+    var seconds = 0
     var timer = Timer()
     var timerIsRunning = false
     var resumeTapped = false
@@ -23,17 +22,26 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var timePicker: UIDatePicker!
     
     @IBAction func startButton(_ sender: UIButton) {
-        runTimer()
+        if timerIsRunning == false {
+            seconds = Int(timePicker.countDownDuration)
+            runTimer()
+            timerIsRunning = true
+            //self.startButtonOutlet.isEnabled = false
+        }
     }
+    
+    @IBOutlet weak var startButtonOutlet: UIButton!
     
     @IBAction func pauseresumeButton(_ sender: UIButton) {
         if self.resumeTapped == false {
             timer.invalidate()
             self.resumeTapped = true
+            self.pauseresumeButtonOutlet.setTitle("Resume", for: .normal)
         }
         else {
             runTimer()
             self.resumeTapped = false
+            self.pauseresumeButtonOutlet.setTitle("Pause", for: .normal)
         }
     }
     
@@ -41,18 +49,28 @@ class FirstViewController: UIViewController {
     
     @IBAction func resetButton(_ sender: UIButton) {
         timer.invalidate()
-        seconds = 60
-        timeDisplay.text = "\(seconds)"
+        seconds = 0
+        timeDisplay.text = timeString(time: TimeInterval(seconds))
+        timerIsRunning = false
+        pauseresumeButtonOutlet.isEnabled = false
     }
     
     //Functions
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(FirstViewController.updateTimer)), userInfo: nil, repeats: true)
+        timerIsRunning = true
+        pauseresumeButtonOutlet.isEnabled = true
     }
     
     @objc func updateTimer() {
-        seconds -= 1 //This will count down seconds.
-        timeDisplay.text = timeString(time: TimeInterval(seconds)) //This will update the label.
+        sendAlert()
+        if seconds < 1 {
+            timer.invalidate()
+        }
+        else {
+            seconds -= 1 //This will count down seconds.
+            timeDisplay.text = timeString(time: TimeInterval(seconds)) //This will update the label.
+        }
     }
     
     func timeString(time:TimeInterval) -> String {
@@ -62,11 +80,33 @@ class FirstViewController: UIViewController {
         
         return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
     }
+    
+    func sendAlert() {
+        if seconds == 0 {
+            let content = UNMutableNotificationContent()
+            content.title = "Your Parking Meter"
+            content.subtitle = "Your Parking Meter is Out of Time"
+            content.body = "0 hours, 0 minutes, 0 seconds"
+            content.badge = 1
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        }
+        if seconds == 120 {
+            //Send Notification/Alert TIME IS AT 2 MIN
+        }
+    }
     //<-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pauseresumeButtonOutlet.isEnabled = false
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in})
     }
 
     override func didReceiveMemoryWarning() {
--- Parking Meter Reminder/Parking Meter Reminder/SecondViewController.swift
+++ Parking Meter Reminder/Parking Meter Reminder/SecondViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var locationMapView: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations location: [CLLocation]) {
+        let location = location[0]
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        locationMapView.setRegion(region, animated: true)
+        
+        self.locationMapView.showsUserLocation = true
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
